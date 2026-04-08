2069bb694c31545db055f2c02ab34a4fb44c462e
Se Joon Park <sparky9043@gmail.com>
2026-04-08 19:45:11 +0900

    feat: define Course and Student models

--- gradebook_main/models.py
+++ gradebook_main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    date_added = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['date_added']
+    
+    def get_year(self):
+        return self.date_added.year
+    
+    def __str__(self):
+        return self.title
+    
+class Student(models.Model):
+    course = models.ManyToManyField(Course)
+    name = models.CharField(max_length=50)
+    level = models.DecimalField(max_digits=2, decimal_places=0)
+    dob = models.DateField()
+    
+    class Meta:
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
