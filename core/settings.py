361ad734ed1a24cd9cb1b4f0dfbe9e50970b8c55
Se Joon Park <sparky9043@gmail.com>
2026-04-08 19:10:32 +0900

    init: add gradebook_main app into settings.py

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # user-installed apps
+    'gradebook_main',
+    
+    # django default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
